79ad01cd04dd802f1a8f923ef1072a4acaefcfc9
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-09-18 20:07:58 -0700

    react added

---
+++ frontend/src/App.js
@@ -0,0 +1,216 @@
+// import image from "../images/image.png"
+import './App.css';
+
+function App() {
+  return (
+    <div class="grid-container">
+    <header class="row">
+        <div>
+            <a class="brand" href="index.html">kukuPay Market Place</a>
+        </div>
+        <div>
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+    <main>
+        <div class="row center">
+            <div class="card">
+                <a href="product.html">
+                    
+                    <img class="medium" src="./images/image.png" alt="product"/>
+
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>iphone 13</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$1200/Tsh 200000</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/nike.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>nike sneakers</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$60</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/shirt.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>men's shirts</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$12</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>t-shirts</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$6</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/sunglasses.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>sunglasses</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$10</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/potatoes.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>potatoes</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa-regular fa-star"></i></span>
+                        <span><i class="fa-regular fa-star"></i></span>
+                        <span><i class="fa-regular fa-star"></i></span>
+                        <span><i class="fa-regular fa-star"></i></span>
+                        <span><i class="fa-regular fa-star"></i></span>
+                    </div>
+                    <div class="price">$50</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>t-shirts</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$6</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>t-shirts</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$6</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>clock</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$3</div>
+                </div>
+            </div>
+            <div class="card">
+                <a href="product.html">
+
+                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                </a>
+                <div class="card-body">
+                    <a href="product.html">
+                        <h2>pants</h2>
+                    </a>
+                    <div class="rating">
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                        <span><i class="fa fa-star"></i></span>
+                    </div>
+                    <div class="price">$50</div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="row center">All rights reserved</footer>
+</div>
+  );
+}
+
+export default App;
