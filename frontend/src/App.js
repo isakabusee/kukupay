d966e807d5202bbca8a5bff242e858d792e2e9bc
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-09-20 11:11:34 -0700

    more initial code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div class="grid-container">
-    <header class="row">
+    <div className="grid-container">
+    <header className="row">
         <div>
-            <a class="brand" href="index.html">kukuPay Market Place</a>
+            <a className="brand" href="index.html">kukuPay Market Place</a>
         </div>
         <div>
             <a href="cart.html">Cart</a>
@@ -14,201 +14,201 @@ function App() {
         </div>
     </header>
     <main>
-        <div class="row center">
-            <div class="card">
+        <div className="row center">
+            <div className="card">
                 <a href="product.html">
                     
-                    <img class="medium" src="./images/image.png" alt="product"/>
+                    <img className="medium" src="./images/image.png" alt="product"/>
 
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>iphone 13</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$1200/Tsh 200000</div>
+                    <div className="price">$1200/Tsh 200000</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/nike.png" alt="product"/>
+                    <img className="medium" src="./images/nike.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>nike sneakers</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$60</div>
+                    <div className="price">$60</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/shirt.png" alt="product"/>
+                    <img className="medium" src="./images/shirt.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>men's shirts</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$12</div>
+                    <div className="price">$12</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                    <img className="medium" src="./images/shirt2.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>t-shirts</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$6</div>
+                    <div className="price">$6</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/sunglasses.png" alt="product"/>
+                    <img className="medium" src="./images/sunglasses.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>sunglasses</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$10</div>
+                    <div className="price">$10</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/potatoes.png" alt="product"/>
+                    <img className="medium" src="./images/potatoes.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>potatoes</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa-regular fa-star"></i></span>
-                        <span><i class="fa-regular fa-star"></i></span>
-                        <span><i class="fa-regular fa-star"></i></span>
-                        <span><i class="fa-regular fa-star"></i></span>
-                        <span><i class="fa-regular fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa-regular fa-star"></i></span>
+                        <span><i className="fa-regular fa-star"></i></span>
+                        <span><i className="fa-regular fa-star"></i></span>
+                        <span><i className="fa-regular fa-star"></i></span>
+                        <span><i className="fa-regular fa-star"></i></span>
                     </div>
-                    <div class="price">$50</div>
+                    <div className="price">$50</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                    <img className="medium" src="./images/shirt2.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>t-shirts</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$6</div>
+                    <div className="price">$6</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                    <img className="medium" src="./images/shirt2.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>t-shirts</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$6</div>
+                    <div className="price">$6</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                    <img className="medium" src="./images/shirt2.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>clock</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$3</div>
+                    <div className="price">$3</div>
                 </div>
             </div>
-            <div class="card">
+            <div className="card">
                 <a href="product.html">
 
-                    <img class="medium" src="./images/shirt2.png" alt="product"/>
+                    <img className="medium" src="./images/shirt2.png" alt="product"/>
                 </a>
-                <div class="card-body">
+                <div className="card-body">
                     <a href="product.html">
                         <h2>pants</h2>
                     </a>
-                    <div class="rating">
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
-                        <span><i class="fa fa-star"></i></span>
+                    <div className="rating">
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
+                        <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div class="price">$50</div>
+                    <div className="price">$50</div>
                 </div>
             </div>
         </div>
     </main>
-    <footer class="row center">All rights reserved</footer>
+    <footer className="row center">All rights reserved</footer>
 </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,125 @@
+const data = {
+    products: [
+        {
+            _id : '1',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '2',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '3',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '4',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '5',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '6',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '7',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '8',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '9',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '10',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id : '11',
+            name: 'iphone 13',
+            category: 'phone',
+            image: '/images/image.png',
+            price: 120,
+            brand: 'Apple',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        }
+    ]
+}
