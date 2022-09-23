f29d862ba91f404e92de44384734d23b67640e2b
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-09-23 15:42:32 -0700

    dynamic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-// import image from "../images/image.png"
+import React from 'react';
+import data from './data';
 import './App.css';
 
 function App() {
@@ -15,15 +16,20 @@ function App() {
     </header>
     <main>
         <div className="row center">
+            {
+                data.products.map((product) => (
+
             <div className="card">
                 <a href="product.html">
                     
-                    <img className="medium" src="./images/image.png" alt="product"/>
+                    {/* <img className="medium" src="./images/image.png" alt="product"/> */}
+                    <img className="medium" src={product.image} alt="product" />
+
 
                 </a>
                 <div className="card-body">
                     <a href="product.html">
-                        <h2>iphone 13</h2>
+                        <h2>{product.name}</h2>
                     </a>
                     <div className="rating">
                         <span><i className="fa fa-star"></i></span>
@@ -32,180 +38,11 @@ function App() {
                         <span><i className="fa fa-star"></i></span>
                         <span><i className="fa fa-star"></i></span>
                     </div>
-                    <div className="price">$1200/Tsh 200000</div>
+                    <div className="price">{product.price}</div>
                 </div>
             </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/nike.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>nike sneakers</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$60</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/shirt.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>men's shirts</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$12</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/shirt2.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>t-shirts</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$6</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/sunglasses.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>sunglasses</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$10</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/potatoes.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>potatoes</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa-regular fa-star"></i></span>
-                        <span><i className="fa-regular fa-star"></i></span>
-                        <span><i className="fa-regular fa-star"></i></span>
-                        <span><i className="fa-regular fa-star"></i></span>
-                        <span><i className="fa-regular fa-star"></i></span>
-                    </div>
-                    <div className="price">$50</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/shirt2.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>t-shirts</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$6</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/shirt2.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>t-shirts</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$6</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/shirt2.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>clock</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$3</div>
-                </div>
-            </div>
-            <div className="card">
-                <a href="product.html">
-
-                    <img className="medium" src="./images/shirt2.png" alt="product"/>
-                </a>
-                <div className="card-body">
-                    <a href="product.html">
-                        <h2>pants</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">$50</div>
-                </div>
-            </div>
+                ))
+            }
         </div>
     </main>
     <footer className="row center">All rights reserved</footer>
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -13,58 +13,58 @@ const data = {
         },
         {
             _id : '2',
-            name: 'iphone 13',
-            category: 'phone',
-            image: '/images/image.png',
-            price: 120,
-            brand: 'Apple',
+            name: 'nike sneakers',
+            category: 'shoes',
+            image: '/images/nike.png',
+            price: 60,
+            brand: 'Nike',
             rating: 4.5,
-            numReviews: 10,
-            description: 'high quality product'
+            numReviews: 11,
+            description: 'high quality shoes'
         },
         {
             _id : '3',
-            name: 'iphone 13',
-            category: 'phone',
-            image: '/images/image.png',
-            price: 120,
-            brand: 'Apple',
-            rating: 4.5,
+            name: 'mens shirts',
+            category: 'clothes',
+            image: '/images/shirt.png',
+            price: 12,
+            brand: 'polo',
+            rating: 4.0,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality mens clothes'
         },
         {
             _id : '4',
-            name: 'iphone 13',
-            category: 'phone',
-            image: '/images/image.png',
-            price: 120,
-            brand: 'Apple',
+            name: 'dress shirt',
+            category: 'clothes',
+            image: '/images/shirt2.png',
+            price: 6,
+            brand: 'cotton shirt',
             rating: 4.5,
-            numReviews: 10,
+            numReviews: 11,
             description: 'high quality product'
         },
         {
             _id : '5',
-            name: 'iphone 13',
-            category: 'phone',
-            image: '/images/image.png',
-            price: 120,
-            brand: 'Apple',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality product'
+            name: 'sunglasses',
+            category: 'eye wear',
+            image: '/images/sunglasses.png',
+            price: 10,
+            brand: 'rayband',
+            rating: 5.0,
+            numReviews: 15,
+            description: 'high end sunglasses'
         },
         {
             _id : '6',
-            name: 'iphone 13',
-            category: 'phone',
-            image: '/images/image.png',
-            price: 120,
-            brand: 'Apple',
-            rating: 4.5,
+            name: 'potatoes',
+            category: 'farm produce',
+            image: '/images/potatoes.png',
+            price: 100,
+            brand: 'farm',
+            rating: 4.8,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality produce from Tanzania'
         },
         {
             _id : '7',
@@ -123,3 +123,4 @@ const data = {
         }
     ]
 }
+export default data;
