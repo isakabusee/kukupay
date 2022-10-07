44f6b92d87abdca9a883c3d68ad83388837193b9
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-10-06 23:23:43 -0700

    rating component is dynamic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from './data';
 import './App.css';
+import Product from './components/Product'
 
 
 function App() {
@@ -8,11 +9,11 @@ function App() {
     <div className="grid-container">
     <header className="row">
         <div>
-            <a className="brand" href="index.html">kukuPay Market Place</a>
+            <a className="brand" href="/">kukuPay Market Place</a>
         </div>
         <div>
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
         </div>
     </header>
     <main>
@@ -20,28 +21,7 @@ function App() {
             {
                 data.products.map((product) => (
 
-            <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                    
-                    {/* <img className="medium" src="./images/image.png" alt="product"/> */}
-                    <img className="medium" src={product.image} alt={product.name} />
-
-
-                </a>
-                <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                        <h2>{product.name}</h2>
-                    </a>
-                    <div className="rating">
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                        <span><i className="fa fa-star"></i></span>
-                    </div>
-                    <div className="price">${product.price}</div>
-                </div>
-            </div>
+           <Product Product key={product._id} product={product}></Product>
                 ))
             }
         </div>
