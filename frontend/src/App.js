52bf5cfb720eba93eafade12444e1b3bda8076ea
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2022-11-02 00:26:29 -0700

    boostrap added and implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,36 @@ import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap'
 
 
 function App() {
   return (
         <Router>
-    <div>
+    <div className="d-flex flex-column site-container">
     <header>
-            <Link to="/">kukuPay Market Place</Link>
+        <Navbar bg="dark" variant="dark">
+                <Container>
+                        <LinkContainer to="/">
+                        <Navbar.Brand>kukuPay Market Place</Navbar.Brand>    
+                        </LinkContainer>
+                </Container>
+        </Navbar>
+
     </header>
     <main>
+        <Container>
             <Routes>
             <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:slug" element={<ProductScreen />} />
 
             </Routes>
+        </Container>
        
     </main>
-    <footer className="row center">All rights reserved</footer>
+    <footer><div className="text-center">All rights reserved</div></footer>
 </div>
         </Router>
   );
