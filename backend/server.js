1584f4e9c2762585b2e53a74b2e56d3eb8a79b78
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-01-18 10:48:02 -0800

    mongodb set up and connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log('connected to db')
+}).catch(error => {
+    console.log(error.message)
+});
 
 const app = express();
 
