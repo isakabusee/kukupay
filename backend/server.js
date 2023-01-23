3b3472363a08af8493ef963dcd8017f4ef19a4cc
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-01-23 11:15:30 -0800

    jwt set and userRoutes created also tested user routes using advanced rest client ARC

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -14,11 +15,20 @@ mongoose.connect(process.env.MONGODB_URI).then(()=>{
 });
 
 const app = express();
+
+// to convert form data into json data in req.body 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 // my APIs below
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
   
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`serve at http://localhost:${port}`);
