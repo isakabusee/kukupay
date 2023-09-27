3ddd0376cb8227e9576cf0e10d61cff96f71f30f
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-09-27 00:36:06 -0700

    orderHistory screen

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -1,11 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import { Helmet } from 'react-helmet-async';
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
+import { Store } from '../Store';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { getError } from '../utils';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_REQUEST':
+            return { ...state,loading: true};
+        case 'FETCH_SUCCESS':
+            return { ...state, orders: action.payload, loading: false };
+        case 'FETCH_FAIL':
+            return { ...state, loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
 export default function OrderHistoryScreen() {
     const { state } = useContext(Store);
     const { userInfo } = state;
+    const navigate = useNavigate();
+    const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+        loading: true, error: '',
+    });
+    
+    useEffect(()=> {
+        const fetchData = async () => {
+            dispatch({ type: 'FETCH_REQUEST' });
+            try {
+                const { data } = await axios.get(
+                    `/api/orders/mine`,
+                    { headers: { Authorization: ` Bearer ${userInfo.token}`} }
+                );
+                dispatch({ type: 'FETCH_SUCCESS', payload: data });
+            } catch (error) {
+                dispatch({ type: 'FETCH_FAIL',
+            payload: getError(error),
+        });
+            }
+        };
+        fetchData();
+    }, [])
   return (
     <div>
         <Helmet>
