ec07b31d2cbb7590bb8edac9ca53aec322670b2f
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-10-10 21:15:00 -0600

    updated api endpoint

--- constants/index.js
+++ constants/index.js
@@ -1,1 +1,6 @@
-export const API_URL = 'https://kukupay-api-rz5n2.ondigitalocean.app';
+let API_URL = "https://kukupay-api-rz5n2.ondigitalocean.app";
+const DEV_URL = "https://kukupay-api-rz5n2.ondigitalocean.app";
+const PROD_URL = "https://kukupay-api-rz5n2.ondigitalocean.app";
+API_URL = process.env.NODE_ENV ? PROD_URL : DEV_URL;
+
+export default API_URL;
