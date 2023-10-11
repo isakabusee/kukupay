60125cbe7a25b2c8b10dc7d4ac716cc9adf37654
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2023-10-10 21:23:45 -0600

    update link

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,7 +7,7 @@ import Product from "../components/Product";
 import { Helmet } from "react-helmet-async";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
-import API_URL from "../../constants";
+import API_URL from "../constants";
 
 const reducer = (state, action) => {
   switch (action.type) {
